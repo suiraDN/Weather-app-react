d059dcd4951f6827a42cafa03d916d9b5a4aa50b
Darius Navickas <dariusnaw@gmail.com>
2022-11-15 21:49:52 +0200

    completed weatherService

--- weather-app/src/services/weatherService.js
+++ weather-app/src/services/weatherService.js
@@ -1,5 +1,7 @@
+import { DateTime } from "luxon";
+
 const API_KEY = "bd5dfab4ca0c3243c4e0b84621977285";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
 	const url = new URL(BASE_URL + "/" + infoType);
@@ -40,20 +42,54 @@ const formatCurrentWeather = (data) => {
 	};
 };
 
+const formatForecastWeather = (data) => {
+	let { timezone, daily, hourly } = data;
+	daily = daily.slice(1, 6).map((d) => {
+		return {
+			title: formatToLocalTime(d.dt, timezone, "ccc"),
+			temp: d.temp.day,
+			icon: d.weather[0].icon,
+		};
+	});
+
+	hourly = hourly.slice(1, 6).map((d) => {
+		return {
+			title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+			temp: d.temp,
+			icon: d.weather[0].icon,
+		};
+	});
+
+	return { timezone, daily, hourly };
+};
+
 const getFormattedWeatherData = async (searchParams) => {
 	const formattedCurrentWeather = await getWeatherData(
 		"weather",
 		searchParams
 	).then(formatCurrentWeather);
+
 	const { lat, lon } = formattedCurrentWeather;
+
 	const formattedForecastWeather = await getWeatherData("onecall", {
 		lat,
 		lon,
 		exclude: "current,minutely,alerts",
 		units: searchParams.units,
-	}).then(formattedForecastWeather);
+	}).then(formatForecastWeather);
 
 	return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
+const formatToLocalTime = (
+	secs,
+	zone,
+	format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) =>
+	`http://openweathermap.org/img/wn/${code}@2x.png`;
+
 export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode };
